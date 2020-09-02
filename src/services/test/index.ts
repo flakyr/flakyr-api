f9a866ce88e672b2502289b0965a02c541095f75
Подкорытов Андрей <a.podkorytov@zarplata.ru>
2020-09-02 15:54:56 +0700

    add getLastBulildIds method in TestService

--- src/services/test/index.ts
+++ src/services/test/index.ts
@@ -1,21 +1,30 @@
+import takeRight from 'lodash/takeRight';
+
 import Test, { TestModel } from 'database/models/test';
 
-export type CreateTestData = {
-    buildId: number;
+export type BuildId = number;
+export type Project = string;
+
+export declare interface ICreateTestData {
+    buildId: BuildId;
     suiteName: string;
     status: boolean;
     testName: string;
-    project: string;
-};
-
-export type Projects = Array<string>;
+    project: Project;
+}
 
 export default class TestService {
-    public static async createTest(data: CreateTestData): Promise<Test> {
-        return TestModel.create<CreateTestData>(data);
+    public static async createTest(data: ICreateTestData): Promise<Test> {
+        return TestModel.create<ICreateTestData>(data);
     }
 
-    public static async getProjects(): Promise<Projects> {
+    public static async getProjects(): Promise<Array<Project>> {
         return TestModel.find().distinct('project');
+    }
+
+    public static async getLastBuildsIds(project: Project, count: number): Promise<Array<BuildId>> {
+        const ids = await TestModel.find({ project }).lean().distinct('buildId');
+
+        return takeRight(ids, count);
     }
 }
